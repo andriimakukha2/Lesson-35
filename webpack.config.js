88df83b7c66a421220c230f962fefcfd4ce72032
andriimakukha2 <andriimakukha2.0@gmail.com>
2024-10-03 12:57:28 +0200

    fixed code

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-    mode: 'development',
     entry: './src/main.ts',
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
@@ -21,56 +21,31 @@ module.exports = {
                 exclude: /node_modules/,
             },
             {
-                test: /\.(scss|css|less)$/,
+                test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader',
-                    'less-loader',
                 ],
             },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|otf)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[hash][ext]',
-                },
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+                test: /\.html$/,
+                loader: 'html-loader',
             },
         ],
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
-        new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
-        new ESLintPlugin({ extensions: ['js', 'ts'], failOnError: true }),
-        new BundleAnalyzerPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
     ],
-    optimization: {
-        splitChunks: { chunks: 'all' },
-    },
     devServer: {
-        static: { directory: path.join(__dirname, 'dist') },
-        compress: true,
+        static: path.join(__dirname, 'dist'),
         port: 9000,
-        open: true,
-        hot: true,
-        watchFiles: ['src/**/*'],
     },
-    resolve: {
-        extensions: ['.js', '.ts', '.json', '.scss', '.css', '.less'],
-    },
+    mode: 'development',
 };
